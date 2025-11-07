9c3e6e35d5899d2420935ebb0c6534484de7d012
Annu Singh <annu37752@gmail.com>
2025-11-08 00:14:16 +0530

    camaras problem

--- Frontend/src/utils/Router.jsx
+++ Frontend/src/utils/Router.jsx
@@ -2,14 +2,13 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from '../page/Home'
 import Session from '../page/Session'
-import NotFound from '../page/NotFound'
 
 const Router = () => {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/session/:unique_id' element={<Session />} />
-      <Route path='/not-found' element={<NotFound />} />
+      
     </Routes>
   )
 }
